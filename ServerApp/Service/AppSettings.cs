6b9fd042d5f4b341c52afa9e0d72843cdd5d7d17
Mikhail Mryasov <damaghack@gmail.com>
2023-08-03 15:50:08 +0700

    Clear Repositry

--- ServerApp/Service/AppSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ServerApp.Service
-{
-    public class AppSettings
-    {
-        public string ipaddress { get; set; }
-        public int port { get; set; }
-        public string connectionString { get; set; }
-    }
-}
